b3560c51fee270505f455c0af4b5aa06088893d8
Nathan C. Castle <nathan.castle@outlook.com>
2017-02-28 11:20:54 -0500

    Adding some scripts to make life easier

---
+++ BSCP/Configurator/mate.sh
@@ -0,0 +1,1 @@
+﻿DISPLAY=:0 exec mate-session
---
+++ BSCP/Configurator/wsl_setup.sh
@@ -0,0 +1,31 @@
+﻿#Note: this script assumes you have Xming or similar installed and configured
+#If not, please install VcXsrv first. If you have chocolatey,try (in powershell): choco install Xming
+
+#note, you'll need to use sudo chmod 777 <filename> to allow the provided run scripts to work
+
+#set up display for xming VcXsrv
+echo "export DISPLAY=:0.0" >> ~/.bashrc
+
+#set up dbus
+sudo sed -i 's$<listen>.*</listen>$<listen>tcp:host=localhost,port=0</listen>$' /etc/dbus-1/session.conf
+sudo sed -i 's$<listen>.*</listen>$<listen>tcp:host=localhost,port=1</listen>$' /etc/dbus-1/system.conf
+
+#install Mate
+
+sudo apt-add-repository ppa:ubuntu-mate-dev/ppa
+sudo apt-add-repository ppa:ubuntu-mate-dev/trusty-mate
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get install mate-desktop-environment-extras
+
+sudo apt-get install unity-settings-daemon #fix for mate-settings-daemon not working
+#http://www.ajopaul.com/2012/05/30/ubuntu-fix-for-unable-to-start-the-settings-manager-mate-settings-daemon/
+
+#install XFCE4
+sudo apt-get install xubuntu-desktop
+
+
+
+#install KDE
+
+#install Gnome
